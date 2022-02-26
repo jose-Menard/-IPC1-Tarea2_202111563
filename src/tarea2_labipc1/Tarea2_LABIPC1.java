4505cc01436a5cad2c0585ed7288914ccf44efa6
jose-Menard <3021892970101@ingenieria.usac.edu.gt>
2022-02-26 11:07:22 -0600

    tarea 2 con imagenes

---
+++ src/tarea2_labipc1/Tarea2_LABIPC1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tarea2_labipc1;
+
+/**
+ *
+ * @author Jose Pablo
+ */
+public class Tarea2_LABIPC1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        Login login = new Login();
+        login.setVisible(true);
+
+
+// TODO code application logic here
+    }
+    
+}
